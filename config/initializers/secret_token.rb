d9ed8dcc2e17effff1eeff254b65269ffb159f2d
Paul Sutcliffe <paul@kosmyka.com>
2012-08-13 11:48:58 -0500

    App ready to start development

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rocknred::Application.config.secret_token = '5c9505010aa6b00491ab26a59bf1067bee5f021a39fb3a38fb6021d255a243e7671a6ea66bd2b341bff0e7fff0df7923d2c69efca143f43b28549d6a865c31f7'
